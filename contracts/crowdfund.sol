77c113ece064ff92e65b93acfee7f058cf85dba6
Biliqis Onikoyi <105673949+BiliqisO@users.noreply.github.com>
2023-10-11 17:09:36 +0100

    foundry

--- contracts/crowdfund.sol
+++ contracts/crowdfund.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
+
 contract CrowdFund{
     struct Campaign{
         uint campaignId;
@@ -11,11 +12,13 @@ contract CrowdFund{
         uint campaignBalance;
         bool isActive;
         address[] contributors;
-        mapping (address => uint) contributorBalance;
+       
     }
+  
+    mapping (uint  =>  mapping(address => uint )) contributorBalance;
     mapping (address => bool)  isContributor;
-    mapping (uint => Campaign) mapCampaign;
-     uint CampaignId;
+    mapping (uint => Campaign) public mapCampaign;
+     uint public CampaignId;
      uint deployTime; 
      address deployer;   
     constructor (){
@@ -23,66 +26,75 @@ contract CrowdFund{
        deployer = msg.sender;
     }
 
-  function createCampaigns(uint _duration, uint _fundingGoal, string memory _campaignTitle) public {
+
+
+  function createCampaigns(uint _duration, uint _fundingGoal, string memory _campaignTitle) public returns (uint){
     CampaignId++;
-    mapCampaign[CampaignId].campaignId = CampaignId;
-    mapCampaign[CampaignId].campaignTitle = _campaignTitle;
-    mapCampaign[CampaignId].duration =   _duration;
-    mapCampaign[CampaignId].fundingGoal = _fundingGoal;
-    mapCampaign[CampaignId].campaignOwner = msg.sender;
-    mapCampaign[CampaignId].isActive = true;
+     Campaign storage  campaign = mapCampaign[CampaignId];
+     campaign.campaignId = CampaignId;
+     campaign.campaignTitle = _campaignTitle;
+     campaign.duration =   _duration;
+     campaign.fundingGoal = _fundingGoal;
+     campaign.campaignOwner = msg.sender;
+     campaign.isActive = true;
+    return (CampaignId);
     }
+    
     modifier activeTime(uint _Id){
         require(block.timestamp <= ( mapCampaign[_Id].duration ) , "time passed");
       _;
     }
     function payCampaign(uint _CampaignId) public payable activeTime(CampaignId) {
-      require(mapCampaign[_CampaignId].isActive, "campaign is no longer active");
-       mapCampaign[ _CampaignId].campaignBalance += msg.value;
-       mapCampaign[ _CampaignId].contributorBalance[msg.sender]+= msg.value;
+      Campaign storage  campaign = mapCampaign[_CampaignId];
+      require(campaign.isActive, "campaign is no longer active");
+        campaign.campaignBalance += msg.value;
+       contributorBalance[_CampaignId][msg.sender] +=msg.value;
        if( isContributor[msg.sender] == false){
-       mapCampaign[ _CampaignId].contributors.push(msg.sender);
+      campaign.contributors.push(msg.sender);
        }
         isContributor[msg.sender] = true;
-
     }
      function refundContributors(uint _CampaignId ) public   returns (string memory ){
-      require(mapCampaign[_CampaignId].duration  < block.timestamp, "time e o yi pe, pada wa ");
-      require(mapCampaign[_CampaignId].campaignBalance < mapCampaign[_CampaignId].fundingGoal, "campaign goal reached");
-      require(mapCampaign[CampaignId].contributorBalance[msg.sender] > 0, "you do not have money in this campaign");
-      mapCampaign[_CampaignId].isActive = false;
+        Campaign storage  campaign = mapCampaign[_CampaignId];
+      require(campaign.duration  < block.timestamp, "time e o yi pe, pada wa ");
+      require(campaign.campaignBalance < campaign.fundingGoal, "campaign goal reached");
+      require(contributorBalance[CampaignId][msg.sender] > 0, "you do not have money in this campaign");
+      campaign.isActive = false;
       string memory successMessage;
-    
-    
-      mapCampaign[CampaignId].campaignBalance   -=   mapCampaign[CampaignId].contributorBalance[msg.sender] ;
-      mapCampaign[CampaignId].contributorBalance[msg.sender] -= mapCampaign[CampaignId].contributorBalance[msg.sender] ;
-      uint userBalance = mapCampaign[CampaignId].contributorBalance[msg.sender];
 
-      for (uint i = 0; i < mapCampaign[_CampaignId].contributors.length; i++) {
-            address contributor = mapCampaign[_CampaignId].contributors[i];
-      payable(contributor).transfer(userBalance);
-            mapCampaign[_CampaignId].contributorBalance[contributor] = 0;
+      for (uint i = 0; i < campaign.contributors.length; i++) {
+          if  (campaign.contributors[i] == msg.sender){
+           address contributor = campaign.contributors[i] ;
+           uint userBalance = contributorBalance[_CampaignId][msg.sender];
+           payable(contributor).transfer(userBalance);
+           contributorBalance[_CampaignId][contributor] -= contributorBalance[_CampaignId][msg.sender] ;
+           campaign.campaignBalance -= userBalance;
         }
+      }
       return (successMessage = "Sorry we have to refund, goal not reached");
      }
-
-     function payCampaignOwner(uint _CampaignId) public  returns ( string memory){
-      require(mapCampaign[_CampaignId].campaignBalance >= mapCampaign[_CampaignId].fundingGoal, "goal not reached");
-      string memory successMessage;
-      address owner = mapCampaign[_CampaignId].campaignOwner;
-      payable(owner).transfer(mapCampaign[_CampaignId].campaignBalance);
-       mapCampaign[_CampaignId].campaignBalance  = 0;   
-      mapCampaign[_CampaignId].isActive = false;
-         return(successMessage = "Congratulations"); 
-         
-     }
-    function getCampaign(uint _campaignId) public view returns( string memory, uint, uint, uint, uint){
-     return (mapCampaign[_campaignId].campaignTitle, mapCampaign[_campaignId].duration,mapCampaign[_campaignId].fundingGoal, mapCampaign[_campaignId].campaignId,   mapCampaign[_campaignId].campaignBalance );
+      function getAllCampaign() public view returns(Campaign[] memory)  {
+      Campaign[] memory all = new Campaign[](CampaignId);
+      for (uint i =0 ; i < CampaignId; i++) {
+        all[i] = mapCampaign[i+1];
+      }
+      return  all; 
     }
-   function getContributors(uint index, uint _CampaignId) public view returns (address) {
-    return  ( mapCampaign[_CampaignId].contributors[index]); 
+     function payCampaignOwner(uint _CampaignId) public  returns ( string memory){
+       Campaign storage  campaign = mapCampaign[_CampaignId];
+      require(campaign.campaignBalance >= campaign.fundingGoal, "goal not reached");
+      string memory successMessage;
+      address owner = campaign.campaignOwner;
+      payable(owner).transfer(campaign.campaignBalance);
+      campaign.campaignBalance  = 0;   
+      campaign.isActive = false;
+      return(successMessage = "Congratulations");   
+     }
+   
+   function getContributors( uint _CampaignId) public view returns (address[] memory) {
+    return  ( mapCampaign[_CampaignId].contributors); 
     }
    function getContributorsBalance(address _addr, uint _CampaignId) public view returns (uint) {
-      return   mapCampaign[_CampaignId].contributorBalance[_addr];
+      return       contributorBalance[_CampaignId][_addr];
    }
 }
